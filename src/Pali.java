1d8939095547e07dfd599789624d78b3e1a97874
romolusi <Eicwe9101>
2019-04-18 00:04:34 +0200

    test

---
+++ src/Pali.java
@@ -0,0 +1,23 @@
+public class Pali {
+
+    public static void main(String[] args) {
+
+        Palindrom_loop palindrom = new Palindrom_loop();
+        palindrom.Palindrom_loop(10);
+    }
+}
+
+class Palindrom_loop {
+
+        public void Palindrom_loop(int InPut_Max) {
+
+                for (int i = 0; i < InPut_Max; i++) {
+                    string_length (i);
+                          }
+        }
+
+        private void string_length (int zahl) {
+            System.out.println(zahl);
+        }
+
+}
